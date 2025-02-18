110a5d8b528599daa91efc6d1f7b196fdfd3d4c4
PlaksinaKate <plaksina.larisa@yandex.ru>
2025-02-18 22:02:14 +0300

    feat: add jest config

---
+++ config/jest/jest.config.ts
@@ -0,0 +1,38 @@
+import path from "path";
+
+export default {
+  globals: {
+    __IS_DEV__: true,
+    __API__: "",
+    __PROJECT__: "jest",
+  },
+
+  clearMocks: true,
+  testEnvironment: "jsdom",
+
+  coveragePathIgnorePatterns: ["/node_modules/"],
+
+  moduleDirectories: ["node_modules"],
+
+  modulePaths: ["<rootDir>/src"],
+
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node",
+  ],
+
+  testMatch: ["<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)"],
+  rootDir: "../../",
+  setupFilesAfterEnv: ["<rootDir>config/jest/setupTests.ts"],
+
+  moduleNameMapper: {
+    "\\.s?css$": "identity-obj-proxy",
+    "\\.svg$": path.resolve(__dirname, "jestEmptyComponent.tsx"),
+  },
+};
